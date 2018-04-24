0fb6cc88463860542da9f1ac78b6a1f0a36484b6
eyaler <eyalgruss@gmail.com>
2018-04-24 04:20:12 +0300

    faster

--- fastface.py
+++ fastface.py
@@ -1,140 +1,155 @@
-#https://www.learnopencv.com/speeding-up-dlib-facial-landmark-detector/
-#https://www.pyimagesearch.com/2017/04/17/real-time-facial-landmark-detection-opencv-python-dlib/
+# see also:
+# https://www.learnopencv.com/speeding-up-dlib-facial-landmark-detector/
+# https://www.pyimagesearch.com/2017/04/17/real-time-facial-landmark-detection-opencv-python-dlib/
 
 from threading import Thread
 import time
 import cv2
 import dlib
 import numpy as np
-from multiprocessing import Process, Queue
-import queue
+from multiprocessing import Process, Value
+import argparse
 
-draw_freq = 3
-fac = 0.5
-src = 0
+cam_src = 0
+mirror = True
+scale_frac = 0.5
+show_face = True
+show_lines = True
+show_marks = False
+line_color = (255, 0, 0)
+mark_color = (255,0,192)
 predictor_path = 'd:/data/faces/shape_predictor_68_face_landmarks.dat'
 
-def shape_to_np(shape, dtype="int"):
-    coords = np.zeros((68, 2), dtype=dtype)
-
-    for i in range(0, 68):
-        coords[i] = (shape.part(i).x, shape.part(i).y)
-
-    return coords
-
-class Detect:
-	def __init__(self):
-		self.land = []
-		self.frame_q = Queue(1)
-		self.land_q = Queue(1)
-		self.detector = dlib.get_frontal_face_detector()
-		self.predictor = dlib.shape_predictor(predictor_path)
-		t = Process(target=self.update, args=())
+class Display:
+	def __init__(self, cam, det):
+		self.terminated = False
+		t = Thread(target=self.update, args=(cam,det))
 		t.daemon = True
 		t.start()
 
-	def get_land(self, img):
-		dets = self.detector(img, 0)
-		shapes = []
-		for k, d in enumerate(dets):
-			p = self.predictor(img, d)
-
-			p = shape_to_np(p)
-			shapes.append(p)
-		return shapes
-
-	def update(self):
-		while True:
-			try:
-				frame = self.frame_q.get_nowait()
-			except queue.Empty:
-				continue
-			land = self.get_land(frame)
-			try:
-				self.land_q.put_nowait(land)
-			except queue.Full:
-				try:
-					self.land_q.get_nowait()
-				except queue.Empty:
-					pass
-				self.land_q.put(land)
-
-	def read(self, frame, fac=1):
-		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		if fac != 1:
-			frame = cv2.resize(frame, None, fx=fac, fy=fac)
-		try:
-			self.frame_q.put_nowait(frame)
-		except queue.Full:
-			try:
-				self.frame_q.get_nowait()
-			except queue.Empty:
-				pass
-			self.frame_q.put(frame)
-		try:
-			self.land = self.land_q.get_nowait()
-		except queue.Empty:
+	def update(self, cam, det):
+		frame_cnt = 0
+		cur_frame = None
+		while cam.frame is None:
 			pass
-		return self.land
+		start = time.time()
+		while not self.terminated:
+			if show_face:
+				cur_frame = cam.frame
+			elif cur_frame is None:
+				cur_frame = np.zeros_like(cam.frame)
+
+			if show_lines or show_marks:
+				cur_faces = det.faces
+				for face in cur_faces:
+					if show_lines:
+						cv2.polylines(cur_frame, [face[:17]], False, line_color)  # Jaw line
+						cv2.polylines(cur_frame, [face[17:22]], False, line_color)  # Left eyebrow
+						cv2.polylines(cur_frame, [face[22:27]], False, line_color)  # Right eyebrow
+						cv2.polylines(cur_frame, [face[27:31]], False, line_color)  # Nose bridge
+						cv2.polylines(cur_frame, [face[30:36]], True, line_color)  # Lower nose
+						cv2.polylines(cur_frame, [face[36:42]], True, line_color)  # Left eye
+						cv2.polylines(cur_frame, [face[42:48]], True, line_color)  # Right Eye
+						cv2.polylines(cur_frame, [face[48:60]], True, line_color)  # Outer lip
+						cv2.polylines(cur_frame, [face[60:68]], True, line_color)  # Inner lip
+
+					if show_marks:
+						for pnt in face:
+							cv2.circle(cur_frame, tuple(pnt[0]), 1, mark_color, -1)
+
+			cv2.imshow('', cur_frame)
+			frame_cnt += 1
+			if cv2.waitKey(1) & 0xFF == 27:
+				break
+
+		delta_t = time.time() - start
+		if delta_t > 0:
+			print('FPS=%.2f' % (frame_cnt / delta_t))
+		self.terminate()
+
+	def terminate(self):
+		if not self.terminated:
+			cv2.destroyAllWindows()
+		self.terminated = True
+
 
 class Camera:
-	def __init__(self, src=0):
+	def __init__(self, src=0, mirror=False):
+		self.src = src
+		self.mirror = mirror
 		self.stream = cv2.VideoCapture(src)
-		self.grabbed, self.frame = self.stream.read()
+		if not self.stream.isOpened():
+			print('Error: camera %d not available' % self.src)
+			self.terminated = True
+			return -1
+		self.terminated = False
+		self.read()
 		t = Thread(target=self.update, args=())
 		t.daemon = True
 		t.start()
 
 	def update(self):
-		while True:
-			self.grabbed, frame = self.stream.read()
-			self.frame = cv2.flip(frame, 1)
+		while not self.terminated:
+			self.read()
 
 	def read(self):
-		return self.frame
+		grabbed, frame = self.stream.read()
+		if not grabbed or frame is None:
+			print('Error: camera %d not available'%self.src)
+			self.terminate()
+		elif self.mirror:
+			self.frame = cv2.flip(frame, 1)
+		else:
+			self.frame = frame
 
-	def stop(self):
-		self.stream.release()
+	def terminate(self):
+		if not self.terminated:
+			self.stream.release()
+		self.terminated = True
 
-def draw_polyline(img, d, start, end, fac=1, closed=False):
-	pts = [np.asarray([[[int(x / fac), int(y / fac)]] for x, y in d[start:end + 1]])]
-	cv2.polylines(img, pts, closed, (255, 0, 0), 1)
+class Detect:
+	def __init__(self, cam_src=0, mirror=False):
+		self.detector = dlib.get_frontal_face_detector()
+		self.predictor = dlib.shape_predictor(predictor_path)
+		self.faces = []
+		self.cam = None
+		self.disp = None
+		self.terminated = Value('b', False)
+		self.external_terminate_signal = Value('b', False)
+		t = Process(target=self.update, args=(cam_src, mirror))
+		t.daemon = True
+		t.start()
 
-def draw_face(img, d, fac):
-	draw_polyline(img, d, 0, 16, fac) # Jaw line
-	draw_polyline(img, d, 17, 21, fac) # Left eyebrow
-	draw_polyline(img, d, 22, 26, fac) # Right eyebrow
-	draw_polyline(img, d, 27, 30, fac) # Nose bridge
-	draw_polyline(img, d, 30, 35, fac, True) # Lower nose
-	draw_polyline(img, d, 36, 41, fac, True) # Left eye
-	draw_polyline(img, d, 42, 47, fac, True) # Right Eye
-	draw_polyline(img, d, 48, 59, fac, True) # Outer lip
-	draw_polyline(img, d, 60, 67, fac, True) # Inner lip
+	def update(self, cam_src=0, mirror=False):
+		self.cam = Camera(cam_src, mirror)
+		self.disp = Display(self.cam, self)
+		while not self.terminated.value:
+			if self.external_terminate_signal.value or (self.cam is not None and self.cam.terminated) or (self.disp is not None and self.disp.terminated):
+				if self.cam is not None:
+					self.cam.terminate()
+				if self.disp is not None:
+					self.disp.terminate()
+				self.terminated.value = True
+				break
+
+			preproc_frame = cv2.cvtColor(self.cam.frame, cv2.COLOR_BGR2GRAY)
+			if scale_frac != 1:
+				preproc_frame = cv2.resize(preproc_frame, None, fx=scale_frac, fy=scale_frac)
+			self.faces = [(np.array([[[p.x, p.y]] for p in self.predictor(preproc_frame, rect).parts()])/scale_frac).astype(int) for rect in self.detector(preproc_frame, 0)]
+
+	def is_terminated(self):
+		return self.terminated.value
+
+	def terminate(self):
+		self.external_terminate_signal.value = True
+
 
 if __name__ == '__main__':
-	frame_cnt = 0
-	cam = Camera(src)
-	det = Detect()
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--src', type=int, help='camera source', default=cam_src)
+	parser.add_argument('--mirror', type=int, help='mirror camera', default=mirror)
+	args = parser.parse_args()
 
-	start = time.time()
-	while True:
-		frame = cam.read()
-		if frame is None:
-			break
-
-		faces = det.read(frame, fac)
-		if frame_cnt % draw_freq == 0:
-			for p in faces:
-				draw_face(frame, p, fac)
-				#for (x, y) in p:
-				#	cv2.circle(frame, (int(x / fac), int(y / fac)), 1, (255, 0, 0), -1)
-
-		cv2.imshow('', frame)
-		if cv2.waitKey(1) & 0xFF == 27:
-			break
-
-		frame_cnt += 1
-
-	print("FPS=%.2f" % (frame_cnt/(time.time()-start)))
-	cv2.destroyAllWindows()
-	cam.stop()
+	det = Detect(args.src, args.mirror)
+	while not det.is_terminated():
+		pass
